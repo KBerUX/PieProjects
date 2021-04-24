81d3f1a2209253aa673d270c3b650b6f05e68631
KBermoy <kristia.bermoy@gmail.com>
2021-04-24 18:28:12 -0500

    new file

--- MadLibs.py
+++
@@ -1,5 +0,0 @@
-# string concatenation --> How to put strings together
-# suppose we want to create a string that says "subscribe to ___"
-youtuber = ""  # some string variable
-
-# ways to do this:
