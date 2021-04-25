fecc4a4735f4a15e3b6ffbd40594f648cdf7d86b
KBermoy <kristia.bermoy@gmail.com>
2021-04-24 23:16:07 -0500

    testing

--- minigame.py
+++ minigame.py
@@ -1,10 +1,15 @@
 # random game world
 
-
 def start_here():
-    user_input = input(
-        "Hi welcome to Random World!: do you want to play?: 'y' for Yes, 'n' for No")
-    if user_input == 'y' or 'Y':
-        print("Let's begin then!")
-    elif user_input == 'n' or 'N':
-        print("Nevermind then!")
+    str.lower(user_input=input(
+        "Hi welcome to Random World!: do you want to play?: 'y' for Yes, 'n' for No: "))
+
+    if user_input == 'y':
+        print("I can't wait to begin with you!")
+    elif user_input == 'n':
+        user_input2 = input("Are you sure?: 'y' for Yes, 'n' for No:")
+    else:
+        user_input3 = input("Only Yes (y) or No (N) please :")
+
+
+print(start_here())
